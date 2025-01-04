6c56a50b43c865b1a22c516a9f3a34b4106048b9
moamenhady <moamenhady@outlook.com>
2025-01-04 23:20:41 +0200

    Add environment.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,23 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { environment } from '../../environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
+    private baseUrl = environment.apiUrl;
+    
     constructor(private http: HttpClient) {}
 
     login(email: string, password: string) {
-        return this.http.post<string>('http://localhost:8080/api/users/login', {
+        return this.http.post<string>(this.baseUrl + '/api/users/login', {
             email: email,
             password: password
         })
     }
 
     register(email: string, password: string) {
-        return this.http.post<string>('http://localhost:8080/api/users/register', {
+        return this.http.post<string>(this.baseUrl + '/api/users/register', {
             email: email,
             password: password
         })
